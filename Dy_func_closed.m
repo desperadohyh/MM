d5979a98f34caf730bfbf4e7a9a6f6bcbc616e03
jessicaleu24 <jess.leu24@berkeley.edu>
2019-08-02 16:13:55 -0700

    Update Dy_func_closed.m

--- Dy_func_closed.m
+++ Dy_func_closed.m
@@ -1,25 +1,18 @@
-dt = 0.1;
+% dt = 0.1;
+% 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%
+% gen_ref_MMD
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% % Arm definition
+% % Arm parameters
+% robot=robotproperty_MMD(4, z0_, Ts);
+% nlink = 5;
+% 
+% 
+% robot.Z0 = sym('z',[19 1]);
+% robot.z0_ = sym('zk',[19 1]);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
-gen_ref_MMD
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Arm definition
-% Arm parameters
-robot=robotproperty_MMD(4, z0_, Ts);
-nlink = 5;
-
-
-robot.Z0 = sym('z',[19 1]);
-robot.z0_ = sym('zk',[19 1]);
-% robot.m = sym('m',[5 1]);
-% robot.l = sym('l',[5 1]);
-% robot.lc = sym('lc',[5 1]);
-% robot.Ixyz = sym('Ixyz',[3,4]);
-% robot.T = sym('T',[3,6]);
-% robot.Tc = sym('Tc',[3,5]);
-% robot.r = sym('r');
-% robot.d = sym('d');
 
 
 % robot.Ic = cell(1,nlink);
@@ -35,6 +28,7 @@ robot.z0_ = sym('zk',[19 1]);
 % Vkf =  matlabFunction(Vk,'File','Vk_f');
 % Gkf =  matlabFunction(Gk,'File','Gk_f');
 %%
+%load('MVG_2.mat')
 ss = 10;
 t = 1:ss;
 torque_implement = [];
@@ -45,10 +39,10 @@ robot.base = [0 0 0]';
 z0_k = [0 0 0 0 0    0 0 0 0 0     0 -pi/2 0 0 0      0 0 0 0 ]';
 % generate refrence
 
-alpha_all = [ (pi)*cos((t/180)*pi*(180)/ss);
+alpha_all = [ 8*(pi)*cos((t/180)*pi*(180)/ss);
               (pi)*cos((t/180)*pi*(180)/ss);
               3*(pi)*cos((t/180)*pi*(180)/ss);
-              (pi/6)*cos((t/180)*pi*(180)/ss);
+              (pi)*cos((t/180)*pi*(180)/ss);
              -(pi/2)*cos((t/180)*pi*(180)/ss);
              -(pi/3)*cos((t/180)*pi*(180)/ss)  ];  
          
@@ -59,18 +53,12 @@ u = alpha_all(:,steps);
 
 [zk, A, B ] = LinKin(z0_k, u, dt);
 
-% Mx_z = [zk(6),zk(12),zk(14),zk(16),zk(18),zk(6),zk(9),zk(11)];
-% Vx_z = [zk(6),zk(7),zk(9),zk(11),zk(12),zk(13),zk(14),zk(15),zk(16),zk(17),zk(18),zk(19),zk(6),zk(9),zk(11),zk(13),zk(15),zk(17),zk(19)];
-% Gv_z = [zk(14),zk(16),zk(18)];
-% robot.z0_ = zk;
-% [ Mk, Vk, Gk, robot] = get_joint_torque_new(robot);
+
 Mk_Z = Mk_f(zk(6),zk(12),zk(14),zk(16),zk(18),zk(6),zk(9),zk(11));
 Vk_Z = Vk_f(zk(6),zk(7),zk(9),zk(11),zk(12),zk(13),zk(14),zk(15),zk(16),zk(17),zk(18),zk(19),zk(6),zk(9),zk(11),zk(13),zk(15),zk(17),zk(19));
 Gk_Z = Gk_f(zk(14),zk(16),zk(18));
 
-% Mk_Z = Mk_f(Mx_z);
-% Vk_Z = Vk_f(Vx_z);
-% Gk_Z = Gk_f(Gx_z);
+
 
 tau = Mk_Z*u+Vk_Z+Gk_Z;
 
@@ -114,4 +102,14 @@ ylabel('Torque')
 % plot(torque_implement1(2,:))
 legend('\theta_1', '\theta_2','\theta_3', '\theta_4','\theta_5', '\theta_6')
 
+figure
+angle_implement_deg = (180/pi).*angle_implement;
+plot(angle_implement_deg(8,:))
+hold on
+plot(angle_implement_deg(10,:))
+plot(angle_implement_deg(12,:))
+plot(angle_implement_deg(14,:))
+plot(angle_implement_deg(16,:))
+plot(angle_implement_deg(18,:))
 
+legend('\theta_1', '\theta_2','\theta_3', '\theta_4','\theta_5', '\theta_6')
